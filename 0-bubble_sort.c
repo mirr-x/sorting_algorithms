ddfc3bec2c6d025190a5257376793f6605e4e51e
amir <lhm76726@gmail.com>
2023-12-20 21:51:07 +0100

    sort-me

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,30 +2,22 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, db = size;
-    int x1, x2;
+    size_t i, j;
+    int temp;
 
-    for (i = 0; i < size; i++)
+    for (i = 0; i < size - 1; i++)
     {
-        if (db - 1 == 0)
+        // Last i elements are already in place, so no need to check them
+        for (j = 0; j < size - i - 1; j++)
         {
-            break;
-        }
-        else if (db - 1 == i)
-        {
-            db--;
-            i = -1;
-            continue;
-        }
-
-        x1 = array[i];
-        x2 = array[i + 1];
-
-        if (x1 > x2)
-        {
-            *(array + i) = x2;
-            *(array + i + 1) = x1;
-            print_array(array, size);
+            if (array[j] > array[j + 1])
+            {
+                // Swap if the current element is greater than the next one
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                print_array(array, size);
+            }
         }
     }
 }
