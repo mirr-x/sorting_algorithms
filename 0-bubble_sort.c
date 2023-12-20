82c2824108f336f76e2d7037a8b7971d0554eace
amir <lhm76726@gmail.com>
2023-12-20 22:02:35 +0100

    sort-me

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,37 @@
 #include "sort.h"
 
+/**
+ * bubble_sort - Sort a array by bubble sort algorithm.
+ * @array: The array to sort.
+ * @size: The size of the array.
+ */
+
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, db = size;
-    int x1, x2;
+	size_t i, db = size;
+	int x1, x2;
 
-    for (i = 0; i < size; i++)
-    {
-        if (db - 1 == 0)
-        {
-            break;
-        }
-        else if (db - 1 == i)
-        {
-            db--;
-            i = -1;
-            continue;
-        }
+	for (i = 0; i < size; i++)
+	{
+		if (db - 1 == 0)
+		{
+			break;
+		}
+		else if (db - 1 == i)
+		{
+			db--;
+			i = -1;
+			continue;
+		}
 
-        x1 = array[i];
-        x2 = array[i + 1];
+		x1 = array[i];
+		x2 = array[i + 1];
 
-        if (x1 > x2)
-        {
-            *(array + i) = x2;
-            *(array + i + 1) = x1;
-            print_array(array, size);
-        }
-    }
+		if (x1 > x2)
+		{
+			*(array + i) = x2;
+			*(array + i + 1) = x1;
+			print_array(array, size);
+		}
+	}
 }
